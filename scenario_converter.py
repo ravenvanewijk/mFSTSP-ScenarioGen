914df3edaa6c0fe38545716bf7d08c1610044470
ravenvanewijk <ravenvanewijk1@gmail.com>
2024-05-02 21:57:05 +0200

    Working for first customerpair

--- scenario_converter.py
+++ scenario_converter.py
@@ -11,6 +11,10 @@ from shapely.ops import linemerge
 
 class mFSTSPRoute:
     def __init__(self, instance_path, sol_type = 'ALL', solutions_name = 'tbl_solutions'):
+        self.route_waypoints = {}
+        self.route_lats = {}
+        self.route_lons = {}
+
         files = os.listdir(instance_path)
         solutions_files = [filename for filename in files if solutions_name in filename]
         self.solutions = {}
@@ -125,45 +129,167 @@ class mFSTSPRoute:
         # zip the customers to create pairs
         cust_pairs = zip(cust_nodes[:-1], cust_nodes[1:])
 
-        for U, V in cust_pairs:
-            # Now use the taxicab package to find the shortest path between 2 customer nodes.
-            # Args come in this order: 0: distance, 1: nodes 2: unfinished linepart begin 3: unfinished linepart end
-            # Look up location of customer nodes U and V in the custnode df
-            routepart = tc.distance.shortest_path(self.G,   (self.custnodes.iloc[U][' latDeg'], 
-                                                        self.custnodes.iloc[U][' lonDeg']), 
-                                                        (self.custnodes.iloc[V][' latDeg'], 
-                                                        self.custnodes.iloc[V][' lonDeg']))
-            
-            # Use the nodes to extract all edges u, v of graph G that the vehicle completely traverses
-            routepart_edges = zip(routepart[1][:-1], routepart[1][1:])
+        # for U, V in cust_pairs:
+        # Now use the taxicab package to find the shortest path between 2 customer nodes.
+        # Args come in this order: 0: distance, 1: nodes 2: unfinished linepart begin 3: unfinished linepart end
+        # Look up location of customer nodes U and V in the custnode df
+        routepart = tc.distance.shortest_path(self.G,   (self.custnodes.iloc[0][' latDeg'], 
+                                                    self.custnodes.iloc[0][' lonDeg']), 
+                                                    (self.custnodes.iloc[19][' latDeg'], 
+                                                    self.custnodes.iloc[19][' lonDeg']))
+        
+        # Remove first and last node from the core list. They are included in the unfinished parts.
+        # routepart[1].pop(0)
+        # routepart[1].pop(-1)
+        # Use the nodes to extract all edges u, v of graph G that the vehicle completely traverses
+        routepart_edges = zip(routepart[1][:-1], routepart[1][1:])
 
-            # routepart
-            route.append(routepart[2])
-            # For every pair of edges, append the route with the Shapely LineStrings
-            for u, v in routepart_edges:
-                # Some edges have this attribute embedded, when geometry is curved
-                if 'geometry' in self.G.edges[(u, v, 0)]:
-                    route.append(self.G.edges[(u, v, 0)]['geometry'])
-                # Other edges don't have this attribute. These are straight lines between their two nodes.
-                else:
-                    # So, get a straight line between the nodes and append that line piece
-                    route.append(LineString([(self.G.nodes[u]['x'], self.G.nodes[u]['y']), 
-                                            (self.G.nodes[v]['x'], self.G.nodes[v]['y'])]))
-            
-            route.append(routepart[3])
+        # routepart at beginning
+        route.append(routepart[2])
+        # For every pair of edges, append the route with the Shapely LineStrings
+        for u, v in routepart_edges:
+            # Some edges have this attribute embedded, when geometry is curved
+            if 'geometry' in self.G.edges[(u, v, 0)]:
+                route.append(self.G.edges[(u, v, 0)]['geometry'])
+            # Other edges don't have this attribute. These are straight lines between their two nodes.
+            else:
+                # So, get a straight line between the nodes and append that line piece
+                route.append(LineString([(self.G.nodes[u]['x'], self.G.nodes[u]['y']), 
+                                        (self.G.nodes[v]['x'], self.G.nodes[v]['y'])]))
+        
+        route.append(routepart[3])
         
         # These are a bunch of lines, these need to combine them into one multilinestring
         route_line = linemerge(route)
 
         # Now that we have the line, we can get the waypoints in LAT LON. 
         # The graph is in LON LAT, but BlueSky works with LAT LON.
-        self.route_waypoints = list(zip(route_line.xy[1], route_line.xy[0]))
-        self.route_lats = route_line.xy[1]
-        self.route_lons = route_line.xy[0]
+        self.route_waypoints[file] = list(zip(route_line.xy[1], route_line.xy[0]))
+        self.route_lats[file] = route_line.xy[1]
+        self.route_lons[file] = route_line.xy[0]
+
+    def construct_scenario(self, file):
+        route_waypoints = self.route_waypoints[file]
+        route_lats = self.route_lats[file]
+        route_lons = self.route_lons[file]
+
+        i = 1 # Start at second waypoint
+        turns = [True] # Doesn't matter what the first waypoint is designated as, so just have it as true.
+        for lat_cur, lon_cur in route_waypoints[1:-1]:
+            # Get the previous and the next waypoint
+            lat_prev, lon_prev = route_waypoints[i-1]
+            lat_next, lon_next = route_waypoints[i+1]
+            # Get the angle
+            a1, _ = kwikqdrdist(lat_prev,lon_prev,lat_cur,lon_cur)
+            a2, _ = kwikqdrdist(lat_cur,lon_cur,lat_next,lon_next)
+            angle=abs(a2-a1)
+            if angle>180:
+                angle=360-angle
+            # In general, we noticed that we don't need to slow down if the turn is smaller than 25 degrees. However, this will depend
+            # on the cruise speed of the drones.
+            if angle > 25:
+                turns.append(True)
+            else:
+                turns.append(False)
+            i+= 1
+
+        # Let the vehicle slow down for the depot
+        turns.append(True)
+
+        # Add some commands to pan to the correct location and zoom in.
+        scen_text = f'00:00:00>PAN {route_lats[0]} {route_lons[0]}\n' # Pan to the origin
+        scen_text += '00:00:00>ZOOM 50\n\n' # Zoom in
+
+        # Get angle of first direction to ensure correct orientation
+        achdg, _ = kwikqdrdist(route_lats[0], route_lons[0], route_lats[1], route_lons[1])
+
+        # CRE command takes Aircraft ID, Aircraft Type, Lat, Lon, hdg, alt[ft], spd[kts]
+        acid = 'TRUCK'
+        actype = 'Truck'
+        acalt = 0 # ft, ground altitude
+        acspd = 5 if turns[1] else 25 #kts, set it as 5 if the first waypoint is a turn waypoint.
+        scen_text += f'00:00:00>CRE {acid} {actype} {route_lats[0]} {route_lons[0]} {achdg} {acalt} {acspd}\n'
+
+        # After creating it, we want to add all the waypoints. We can do that using the ADDTDWAYPOINTS command.
+        # ADDTDWAYPOINTS can chain waypoint data in the following way:
+        # ADDTDWAYPOINTS ACID LAT LON ALT SPD Turn? TurnSpeed
+        # SPD here can be set as the cruise speed so the vehicle knows how fast to go
+        cruise_spd = 25 #kts
+        cruise_alt = acalt # Keep it constant throughout the flight
+        # Turn speed of 5 kts usually works well
+        turn_spd = 5 #kts
+
+        # So let's create this command now
+        scen_text += f'00:00:00>ADDTDWAYPOINTS {acid}' # First add the root of the command
+        # Then start looping through waypoints
+        for wplat, wplon, turn in zip(route_lats, route_lons, turns):
+            # Check if this waypoint is a turn
+            if turn:
+                wptype = 'TURNSPD'
+            else:
+                wptype = 'FLYBY'
+            # Add the text for this waypoint. It doesn't matter if we always add a turn speed, as BlueSky will
+            # ignore it if the wptype is set as FLYBY
+            scen_text += f',{wplat},{wplon},{cruise_alt},{cruise_spd},{wptype},{turn_spd}'
+
+        # Add a newline at the end of the addwaypoints command
+        scen_text += '\n'
+
+        # Now we just need to make sure that the aircraft is configured properly. So we enable its vertical and horizontal navigation
+        scen_text += f'00:00:00>LNAV {acid} ON\n'
+        scen_text += f'00:00:00>VNAV {acid} ON\n'
+        # Turn trail on, tracing for testing
+        scen_text += '00:00:00>TRAIL ON \n'
+        scen_text += f'00:00:00>POS {acid}\n'
+        # We also want the aircraft removed when it reaches the destination. We can use an ATDIST command for that.
+        destination_tolerance = 3/1852 # we consider it arrived if it is within 3 metres of the destination, converted to nautical miles.
+        scen_text += f'00:00:00>{acid} ATDIST {route_lats[-1]} {route_lons[-1]} {destination_tolerance} DEL {acid}\n'
+
+        # # Change directory to scenario folder
+        # try:
+        #     os.chdir(root_path + '/scenario')
+        # except:
+        #     raise Exception('Scenario folder not found')
+
+        # Now we save the text in a scenario file
+        with open('RTMtest.scn', 'w') as f:
+            f.write(scen_text)
+
+
+def kwikqdrdist(lata, lona, latb, lonb):
+    """Gives quick and dirty qdr[deg] and dist [m]
+       from lat/lon. (note: does not work well close to poles)"""
+    re      = 6371000.  # radius earth [m]
+    dlat    = np.radians(latb - lata)
+    dlon    = np.radians(((lonb - lona)+180)%360-180)
+    cavelat = np.cos(np.radians(lata + latb) * 0.5)
+
+    dangle  = np.sqrt(dlat * dlat + dlon * dlon * cavelat * cavelat)
+    dist    = re * dangle
+
+    qdr     = np.degrees(np.arctan2(dlon * cavelat, dlat)) % 360
+    return qdr, dist
+
 
 routes = mFSTSPRoute("/Users/raven/Documents/TU/MSc/Thesis/Code/mFSTSP/Problems/20170606T123331779163",
                      "tbl_solutions_103_1_Heuristic.csv")
 routes.construct_route("tbl_solutions_103_1_Heuristic.csv")
-
+routes.construct_scenario("tbl_solutions_103_1_Heuristic.csv")
 # routes = mFSTSPRoute("/Users/raven/Documents/TU/MSc/Thesis/Code/mFSTSP/Problems/20170606T113038113409",
 #                      "tbl_solutions_103_1_Heuristic.csv")
+
+
+
+# <MULTILINESTRING ((-78.812 42.91, -78.813 42.91, -78.814 42.91, -78.814 42.9...>
+# route_line.xy
+# Traceback (most recent call last):
+#   File "<string>", line 1, in <module>
+#   File "/Users/raven/Documents/TU/MSc/Thesis/Code/mFSTSP-ScenarioGen/.venv/lib/python3.9/site-packages/shapely/geometry/base.py", line 229, in xy
+#     raise NotImplementedError
+# NotImplementedError
+
+
+# route_line
+# <LINESTRING (4.471 51.923, 4.473 51.924, 4.475 51.924, 4.478 51.924, 4.48 51...>
+# route_line.xy
+# (array('d', [4.470622761691262, 4.473167643594631, 4.474864219123583, 4.47788480878786..., 4.4911524452493135, 4.4914243281920285]), array('d', [51.92291411691797, 51.923533842070725, 51.923856414675406, 51.92443055982...09, 51.92056518330626, 51.92067251358025]))
