714dd0c40c580aad0c4c6b65de7e13e82e355400
ravenvanewijk <ravenvanewijk1@gmail.com>
2024-10-01 16:49:50 +0200

    30 buffalo instances

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # args.input_dir = '../mFSTSP/Problems/20170606T113038113409'
     # args.sol_file = 'tbl_solutions_101_1_Heuristic.csv'
-    args.uncertainty = False
+    args.uncertainty = 'heavy'
     input_arr = main(args.input_dir, args.sol_file, args.uncertainty)
 
     # for inp in input_arr:
